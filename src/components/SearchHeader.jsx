5e92cf9bd8b29f36bb5b56804e92825ad3ba9912
coder.yoon <yjin830201@gmail.com>
2022-11-27 22:59:15 +0900

    styling with tailwindcss

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -14,19 +14,20 @@ export default function SearchHeader() {
   useEffect(() => setText(keyword || ''), [keyword])
 
   return (
-    <header>
-      <Link to="/">
-        <BsYoutube />
-        <h1>Youtube</h1>
+    <header className="w-full flex p-4 text-2xl border-b border-zinc-600 mb-4">
+      <Link to="/" className="flex items-center">
+        <BsYoutube className="text-4xl text-brand" />
+        <h1 className="font-bold ml-2 text-3xl">Youtube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form className="w-full flex justify-center" onSubmit={handleSubmit}>
         <input
+          className="w-7/12 p-2 outline-none bg-black text-gray-50"
           type="text"
           placeholder="Search..."
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button>
+        <button className="bg-zinc-600 px-4">
           <BsSearch />
         </button>
       </form>
