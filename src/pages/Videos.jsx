136a590985461db4f825b1db99f16ace59ba2062
coder.yoon <yjin830201@gmail.com>
2022-11-30 00:40:31 +0900

    change fetch to axios

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,15 +2,16 @@ import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import VideoCard from '../components/VideoCard'
+import axios from 'axios'
 
 export default function Videos() {
   const { keyword } = useParams()
   const { isLoading, error, data: videos } = useQuery(
     ['videos', keyword],
     async () => {
-      return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-        .then((res) => res.json())
-        .then((data) => data.items)
+      return axios
+        .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        .then((res) => res.data.items)
     },
   )
   return (
