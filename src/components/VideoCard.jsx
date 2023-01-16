0bfde2f743964a98a7d58818f08ee410888d705e
coder.yoon <yjin830201@gmail.com>
2023-01-16 22:28:15 +0900

    create video card detail and put some design with css

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { formatAgo } from '../util/date'
 
 export default function VideoCard({ video }) {
-  return <div>{video.snippet.title}</div>
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet
+  return (
+    <li>
+      <img className="w-full" src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p className="font-semibold my-2 line-cramp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{formatAgo(publishedAt, 'ko')}</p>
+      </div>
+    </li>
+  )
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -16,7 +16,7 @@ export default function Videos() {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 😠</p>}
       {videos && (
-        <ul>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
